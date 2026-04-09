448bad039247b2ec9fef6b08e39cbcd531298691
hafizaaisra-cmyk <hafizaaisra@gmail.com>
2026-04-09 23:05:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# --- CONFIGURATION ---
+# --- CONFIGURATION ----
 st.set_page_config(page_title="Supplier Risk System", layout="wide")
 
 # --- STYLING ---
